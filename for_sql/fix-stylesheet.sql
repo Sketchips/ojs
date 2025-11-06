788fd9f24502140585e505c749eb4fdd05f6460d
Angeleo Ivan Cahya Pratama <angvacp@gmail.com>
2025-11-06 09:04:45 +0700

    delet

--- for_sql/fix-stylesheet.sql
+++
@@ -1,4 +0,0 @@
--- Fix styleSheet database entry
-UPDATE journal_settings 
-SET setting_value = '{"uploadName":"styleSheet.css","dateUploaded":"2025-10-30 09:00:00"}' 
-WHERE journal_id = 1 AND setting_name = 'styleSheet';
--- for_sql/update-css-v10.5.sql
+++
@@ -1,3 +0,0 @@
-UPDATE journal_settings 
-SET setting_value = '{"uploadName": "styleSheet.css", "dateUploaded": "2025-01-04 15:30:00"}'
-WHERE setting_name = 'styleSheet' AND journal_id = 1;
